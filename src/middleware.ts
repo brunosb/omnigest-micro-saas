a40ee0ced5e080ec64d4d9469f1abd6edb57319f
Bruno Silva Barbosa <barbosa.developer@gmail.com>
2024-09-07 03:34:14 -0300

    remove console.log in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,6 @@ export function middleware(request: NextRequest) {
 	const token = request.cookies.get('next-auth.session-token')
 	const pathname = request.nextUrl.pathname
 
-	console.log('token', token)
-	console.log('pathname', pathname)
-
 	if (token && pathname.startsWith('/auth')) {
 		return NextResponse.redirect(new URL('/app', request.url))
 	}
